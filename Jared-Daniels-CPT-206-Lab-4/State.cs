1283ed31ad117457bde6ded67b458eb207f8bd30
Jared Daniels <148314385+YoungRager24@users.noreply.github.com>
2025-02-14 12:31:31 -0500

    Lab 4

---
+++ Jared-Daniels-CPT-206-Lab-4/State.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jared_Daniels_CPT_206_Lab_4
+{
+    public class State
+    {
+        public int ID { get; set; }
+        public string StateName { get; set; }
+        public int Population { get; set; }
+        public string StateFlagDescription { get; set; }
+        public string StateFlower { get; set; }
+        public string StateBird { get; set; }
+        public string Colors { get; set; }
+        public string LargestCities { get; set; }
+        public string Capital { get; set; }
+        public decimal MedianIncome { get; set; }
+        public double ComputerRelatedJobsPercentage { get; set; }
+    }
+}
